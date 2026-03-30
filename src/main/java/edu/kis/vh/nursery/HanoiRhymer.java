1ab4882db7b437dacbc6723b32b3510134309d98
255684 <255684@edu.p.lodz.pl>
2026-03-30 13:13:11 +0200

    Accessory i hermetyzacja + poprawienie testow jednostkowych (literowki w nazwach)

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    private int totalRejected = 0;
 
     public int reportRejected() {
         return totalRejected;
@@ -15,5 +15,4 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
         else
             super.countIn(in);
     }
-
 }
